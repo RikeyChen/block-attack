7f10d8592ebd4ab4f49062272055982228515883
Rikey Chen <rikey.chen16@gmail.com>
2019-01-10 09:38:37 -0800

    Add basic gameOver functionality

--- src/game/board.js
+++ src/game/board.js
@@ -1,5 +1,14 @@
+const level = [undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined];
+
+const defaultGrid = [];
+
+// Make default grid 21 rows in height
+for (let i = 0; i < 21; i++) {
+  defaultGrid.push(Array.from(level));
+}
+
 class Board {
-  constructor(grid) {
+  constructor(grid = defaultGrid) {
     this.grid = grid;
     this.gameOver = this.gameOver.bind(this);
     this.fullBoard = this.fullBoard.bind(this);
@@ -10,11 +19,17 @@ class Board {
   }
 
   fullBoard() {
-    this.grid[20];
-  }
-
-  row() {
-    this.grid;
+    for (let i = 0; i < 10; i++) {
+      if (
+        this.grid[i][0]
+      && this.grid[i][1]
+      && this.grid[i][2]
+      && this.grid[i][3]
+      && this.grid[i][4]
+      ) {
+        return true;
+      } return false;
+    }
   }
 }
 
