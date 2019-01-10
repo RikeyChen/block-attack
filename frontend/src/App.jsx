878357ea7ee79c409615dd8d1e275e5d8f75ddaf
Rikey Chen <rikey.chen16@gmail.com>
2019-01-09 18:00:34 -0800

    Initial structure creation and render background

---
+++ frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './stylesheets/App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <div id='stars'></div>
+          <div id='stars2'></div>
+          <div id='stars3'></div>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ frontend/src/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './stylesheets/index.css';
+import App from './App';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const root = document.getElementById('root');
+  ReactDOM.render(<App />, root);
+})
