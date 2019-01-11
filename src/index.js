e054b43d72e1e0e4922b72c15acf4f84b36abc40
Rikey Chen <rikey.chen16@gmail.com>
2019-01-11 11:04:00 -0800

    Add renderBlocks

--- src/index.js
+++ src/index.js
@@ -4,13 +4,16 @@ class App {
   constructor(game = new Game()) {
     this.game = game;
     this.populateGrid = this.populateGrid.bind(this);
+    this.board = this.game.board;
+    this.renderBlocks = this.renderBlocks.bind(this);
+
+    this.populateGrid();
   }
 
   populateGrid() {
-    const { board } = this.game;
-    const gameContainer = document.getElementsByClassName('game-container')[0];
+    const gameContainer = document.getElementsByClassName('block-game')[0];
 
-    board.grid.forEach((row, idx1) => {
+    this.board.grid.forEach((row, idx1) => {
       const rowNode = document.createElement('div');
       rowNode.className = `row ${idx1}`;
 
@@ -23,11 +26,25 @@ class App {
       gameContainer.appendChild(rowNode);
     });
   }
+
+  renderBlocks() {
+    const { grid } = this.board;
+
+    grid.forEach((row, idx1) => {
+      row.forEach((col, idx2) => {
+        const pos = document.getElementsByClassName(`pos-${idx1}-${idx2}`)[0];
+        if (grid[idx1][idx2] !== 'X') {
+          pos.className += ` ${this.game.currentBlock.symbol}`;
+        }
+      });
+    });
+  }
 }
 
 export default App;
 
 document.addEventListener('DOMContentLoaded', () => {
   const app = new App();
-  app.populateGrid();
+
+  app.renderBlocks();
 });
