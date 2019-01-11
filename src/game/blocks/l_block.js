5ce661bbb6c9e90c6e4b1803ec7401bdd2dc4257
Rikey Chen <rikey.chen16@gmail.com>
2019-01-11 11:09:27 -0800

    Correct startPos name

--- src/game/blocks/l_block.js
+++ src/game/blocks/l_block.js
@@ -4,9 +4,9 @@ class LBlock extends Block {
   constructor(type = 'fours') {
     super(type);
     this.symbol = 'L';
-    this.pos = [[1, 0], [1, 1], [1, 2], [0, 2]];
+    this.startPos = [[1, 4], [1, 5], [1, 6], [0, 6]];
     this.currentPos = this.startPos;
-    this.pos = [[1, 4], [1, 5], [1, 6], [0, 6]];
+    this.pos = [[1, 0], [1, 1], [1, 2], [0, 2]];
   }
 }
 
