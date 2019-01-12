a9525e49056a9ddcb2df189c3388604a0d56451f
Rikey Chen <rikey.chen16@gmail.com>
2019-01-11 23:13:15 -0800

    Reorder startPos

--- src/game/blocks/s_block.js
+++ src/game/blocks/s_block.js
@@ -4,7 +4,7 @@ class SBlock extends Block {
   constructor(type = 'threes') {
     super(type);
     this.symbol = 'S';
-    this.startPos = [[1, 3], [1, 4], [0, 4], [0, 5]];
+    this.startPos = [[0, 4], [0, 5], [1, 3], [1, 4]];
     this.currentPos = this.startPos;
     this.pos = [[1, 0], [1, 1], [0, 1], [0, 2]];
   }
