bdacc1ed4d108a497418f8d36f3ca91897d975a2
Rikey Chen <rikey.chen16@gmail.com>
2019-01-11 22:52:04 -0800

    Reorder startPos for game over functionality

--- src/game/blocks/t_block.js
+++ src/game/blocks/t_block.js
@@ -4,7 +4,7 @@ class TBlock extends Block {
   constructor(type = 'fours') {
     super(type);
     this.symbol = 'T';
-    this.startPos = [[1, 4], [1, 5], [0, 5], [1, 6]];
+    this.startPos = [[0, 5], [1, 4], [1, 5], [1, 6]];
     this.currentPos = this.startPos;
     this.pos = [[1, 0], [1, 1], [0, 1], [1, 2]];
   }
