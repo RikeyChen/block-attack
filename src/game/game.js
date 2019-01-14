860e7ea4699b78ed99f608697f1c5e76a35a29d2
Rikey Chen <rikey.chen16@gmail.com>
2019-01-13 22:44:17 -0800

    Add score and level functionality

--- src/game/game.js
+++ src/game/game.js
@@ -5,11 +5,19 @@ class Game {
     this.board = board;
     this.over = this.over.bind(this);
     this.playNextBlock = this.playNextBlock.bind(this);
-    this.time = 1000;
+    this.level = 1;
+    this.time = (this.level === 1 ? 1000 : (1000 / (2 * this.level)));
+    this.score = 0;
+    this.updateLevel = this.updateLevel.bind(this);
+    this.updateScore();
+    this.updateLevel();
+
     document.addEventListener('keydown', (e) => {
       switch (e.key) {
         case 'ArrowDown':
           this.board.shiftBlock(this.board.currentBlock, 'down');
+          this.score += 40 * this.level;
+          this.updateScore();
           break;
         case 'ArrowUp':
           this.board.rotateBlock(this.board.currentBlock);
@@ -30,7 +38,30 @@ class Game {
     return this.board.gameOver;
   }
 
+  scoringByLinesCleared() {
+    return {
+      1: 40,
+      2: 100,
+      3: 300,
+      4: 1200,
+    };
+  }
+
+  updateLevel() {
+    const level = document.getElementsByClassName('level')[0];
+    level.innerHTML = `Level ${this.level}`;
+  }
+
+  updateScore() {
+    const score = document.getElementsByClassName('score')[0];
+    score.innerHTML = `Score: ${this.score}`;
+  }
+
   playNextBlock() {
+    if (this.score >= ((10000 * 2.5) * (2 ** this.level))) {
+      this.level += 1;
+      this.updateLevel();
+    }
     const { currentBlock } = this.board;
     if (currentBlock.start && this.board.blockRenderable(currentBlock)) {
       this.board.renderBlockStart(currentBlock);
@@ -40,6 +71,8 @@ class Game {
       if (!this.board.shiftable(currentBlock, 'down')) {
         this.board.clearRows();
         if (this.board.rowClearCount > 0) {
+          this.score += this.scoringByLinesCleared()[this.board.rowClearCount] * (this.level + 1);
+          this.updateScore();
           const shiftClearedRows = setInterval(() => {
             this.board.shiftClearedRow();
             if (this.board.rowClearCount === 0) {
